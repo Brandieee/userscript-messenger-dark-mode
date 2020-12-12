0fbb58ab73ccf1e42ac6ab5bb6ebe4aedb2a55da
David <david+git@uhlerbrand.se>
2020-12-12 20:36:54 +0100

    Added update url

--- userscript-messenger-dark-mode.meta.js
+++ userscript-messenger-dark-mode.meta.js
@@ -10,21 +10,3 @@
 // @updateURL https://raw.githubusercontent.com/Brandieee/userscript-messenger-dark-mode/main/userscript-messenger-dark-mode.meta.js
 // @downloadURL https://raw.githubusercontent.com/Brandieee/userscript-messenger-dark-mode/main/userscript-messenger-dark-mode.js
 // ==/UserScript==
-
-window.addEventListener('load', (event) => {
-  changeStylesheet();
-});
-
-
-function changeStylesheet() {
-  if (document.styleSheets.length >= 4){
-      
-    sep = document.styleSheets[2].cssRules[1].cssText.indexOf(':')
-
-    document.styleSheets[3].insertRule(document.styleSheets[2].cssRules[1].cssText.slice(sep))
-    document.styleSheets[3].deleteRule(1)
-  } else {
-    console.log("Waiting")
-    setTimeout(changeStylesheet, 300);
-  }
-}
