f2150077876a74ab7ad2f716b0517928e60752af
David <david+git@uhlerbrand.se>
2020-12-12 20:24:22 +0100

    Initial commit

---
+++ userscript-messenger-dark-mode.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        Change stylesheet to messenger dark mode
+// @namespace   Scripts
+// @include     https://www.messenger.com/*
+// @grant       none
+// @run-at      document-end
+// @version     1.0
+// @author      Brandieee
+// @description 12/12/2020, 7:14:52 PM
+// @downloadURL https://github.com/Brandieee/userscript-messenger-dark-mode/blob/main/userscript-messenger-dark-mode.js
+// ==/UserScript==
+
+window.addEventListener('load', (event) => {
+  changeStylesheet();
+});
+
+
+function changeStylesheet() {
+  if (document.styleSheets.length >= 4){
+      
+    sep = document.styleSheets[2].cssRules[1].cssText.indexOf(':')
+
+    document.styleSheets[3].insertRule(document.styleSheets[2].cssRules[1].cssText.slice(sep))
+    document.styleSheets[3].deleteRule(1)
+  } else {
+    console.log("Waiting")
+    setTimeout(changeStylesheet, 300);
+  }
+}
