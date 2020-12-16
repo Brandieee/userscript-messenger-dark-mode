784e4451611f8f4b3966fe4115ffe9b988da4025
David <david+git@uhlerbrand.se>
2020-12-16 21:51:08 +0100

    Updated to changed stylesheet order

--- userscript-messenger-dark-mode.js
+++ userscript-messenger-dark-mode.js
@@ -4,9 +4,9 @@
 // @include     https://www.messenger.com/*
 // @grant       none
 // @run-at      document-end
-// @version     1.0
+// @version     1.0.1
 // @author      Brandieee
-// @description 12/12/2020, 7:14:52 PM
+// @description 16/12/2020
 // @updateURL https://raw.githubusercontent.com/Brandieee/userscript-messenger-dark-mode/main/userscript-messenger-dark-mode.meta.js
 // @downloadURL https://raw.githubusercontent.com/Brandieee/userscript-messenger-dark-mode/main/userscript-messenger-dark-mode.js
 // ==/UserScript==
@@ -19,10 +19,9 @@ window.addEventListener('load', (event) => {
 function changeStylesheet() {
   if (document.styleSheets.length >= 4){
       
-    sep = document.styleSheets[2].cssRules[1].cssText.indexOf(':')
-
-    document.styleSheets[3].insertRule(document.styleSheets[2].cssRules[1].cssText.slice(sep))
-    document.styleSheets[3].deleteRule(1)
+    sep = document.styleSheets[0].cssRules[1].cssText.indexOf(':')
+	document.styleSheets[0].insertRule(document.styleSheets[0].cssRules[1].cssText.slice(sep))
+	document.styleSheets[0].deleteRule(1)
   } else {
     console.log("Waiting")
     setTimeout(changeStylesheet, 300);
