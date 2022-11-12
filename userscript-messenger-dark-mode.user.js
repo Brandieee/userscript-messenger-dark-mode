924a44b087420b5a4e5334eef47ffbe06dce3f52
David <10778583+Brandieee@users.noreply.github.com>
2022-11-12 23:01:25 +0100

    Works with TamperMonkey v4.18.0 and messenger (2022-11-12)

--- userscript-messenger-dark-mode.user.js
+++ userscript-messenger-dark-mode.user.js
@@ -4,23 +4,24 @@
 // @include     https://www.messenger.com/*
 // @grant       none
 // @run-at      document-end
-// @version     1.0.5
+// @version     1.1.0
 // @author      Brandieee
 // @description 16/10/2021
 // @updateURL https://raw.githubusercontent.com/Brandieee/userscript-messenger-dark-mode/main/userscript-messenger-dark-mode.meta.js
 // @downloadURL https://raw.githubusercontent.com/Brandieee/userscript-messenger-dark-mode/main/userscript-messenger-dark-mode.user.js
 // ==/UserScript==
 
-window.addEventListener('load', (event) => {
+(function() {
+  'use strict';
   changeStylesheet();
-});
 
 
-function changeStylesheet() {
-  if (document.styleSheets.length >= 1){
-	  document.styleSheets[0].deleteRule(2)
-  } else {
-    console.log("Waiting")
-    setTimeout(changeStylesheet, 300);
+  function changeStylesheet() {
+      if (document.styleSheets.length >= 1){
+          document.styleSheets[0].deleteRule(2)
+      } else {
+          console.log("Waiting")
+          setTimeout(changeStylesheet, 300);
+      }
   }
-}
+})();
